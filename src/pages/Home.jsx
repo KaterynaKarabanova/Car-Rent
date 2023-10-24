f646d4cb687e8748986981ea8e6e5924e0f067fe
Kateryna <ikatya7575@gmaiil.com>
2023-10-24 11:27:59 +0300

    swiiper hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,48 @@
-import { AnimateKeyframes } from 'react-simple-animate';
 import { StyledBack } from './Catalog/Catalog.styled';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
 
 const Home = () => {
+  const swiperParams = {
+    direction: 'horizontal', // Set the direction to horizontal
+    slidesPerView: 1, // Display one slide at a time
+    spaceBetween: 20, // Adjust the space between slides as needed
+  };
+
   return (
     <StyledBack>
-      <div>
-        <AnimateKeyframes
-          play={true}
-          pause={false}
-          iterationCount="infinite"
-          direction="alternate"
-          duration={20}
-          keyframes={[
-            'transform: rotateX(0) rotateY(0) rotateZ(0)',
-            'transform: rotateX(360deg) rotateY(360deg) rotateZ(360deg)',
-          ]}
-        >
+      <Swiper {...swiperParams}>
+        <SwiperSlide>
+          <img
+            src="https://pngimg.com/d/mercedes_PNG80188.png"
+            alt="hhh"
+            style={{ width: '80%' }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://pngimg.com/d/mercedes_PNG80135.png"
+            alt="hhh"
+            style={{ width: '80%' }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
           <img
             src="https://www.kia.com/content/dam/kia/us/en/home2-0/mtf-carousel/sedan/k5/kia_k5_2024_large-middle.png"
             alt="hhh"
-            width={200}
-            height={200}
+            style={{ width: '80%' }}
           />
-        </AnimateKeyframes>
-      </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://images.carprices.com/pricebooks_data/usa/colorized/2022/Mercedes-Benz/View2/GLS/GLS_450/GLS450W4_040.png"
+            alt="hhh"
+            style={{ width: '80%' }}
+          />
+        </SwiperSlide>
+      </Swiper>
     </StyledBack>
   );
 };
+
 export default Home;
